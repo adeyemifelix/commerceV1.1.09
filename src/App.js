281d8d0d72a911c8e5f048d328579c2326067475
adeyemifelix <58184393+adeyemifelix@users.noreply.github.com>
2020-07-13 14:52:56 +0100

    broken the homepage into smaller components, and Styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-//import {Route} from 'react-router-dom';
-import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 
 function App() {
   return (
-    <div>     
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route  path='/hats' component={HatsPage}/>
-      </Switch>  
-    </div>
+    <HomePage/>
   );
 }
 
