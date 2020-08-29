73ebef3b5a4dd54a2b8afb9a81cd8a51e042bcea
adeyemifelix <58184393+adeyemifelix@users.noreply.github.com>
2020-08-29 02:34:38 +0100

    Finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -12,6 +12,31 @@ const config = {
     appId: "1:949935321894:web:8941402e1c8820ceb607e7",
     measurementId: "G-JRZE41SRDG"
   };
+ 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch (error){
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
+//import Signup from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        <SignIn/>
+        <SignIn />
+        <SignUp />
     </div>
 )
 
